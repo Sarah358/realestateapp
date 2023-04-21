d8f1ba5602ba7a2d9b86069cab6c192150d7e801
Sarah358 <njerinjuguna358@gmail.com>
2023-04-21 19:59:19 +0300

    add properties and enquiries apps

---
+++ apps/enquiries/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Enquiry
+
+# Register your models here.
+class EnquiyAdmin(admin.ModelAdmin):
+    list_display = ["name","email","phone_number","message"]
+
+admin.site.register(Enquiry,EnquiyAdmin)
