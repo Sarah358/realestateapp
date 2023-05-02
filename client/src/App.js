0aa4fe614f2cf8415148acae31093fabfe84b52d
Sarah358 <njerinjuguna358@gmail.com>
2023-05-02 13:40:56 +0300

    chore:setup nginx and react with docker

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Real Estate</h1>
+      <p>The real test start here</p>
+     
+    </div>
+  );
+}
+
+export default App;
