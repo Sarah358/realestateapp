b07e131a8db09aa0462c6667935a0c1f4e7be416
Sarah358 <njerinjuguna358@gmail.com>
2023-05-02 12:55:51 +0300

    ensure all containers work

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -25,9 +25,9 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 DJANGO_APPS = [
+    'django.contrib.contenttypes',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -41,7 +41,8 @@ THIRD_PARTY_APPS = [
     'django_countries',
     'phonenumber_field',
     'djoser',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'djcelery_email'
 
 ]
 LOCAL_APPS = [
--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # EMAIL BACKEND CONFIGURATIONS
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
@@ -24,3 +24,7 @@ DATABASES = {
         
     }
 }
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "Africa/Kigali"
